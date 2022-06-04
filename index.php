b0e8e9bdbf96d14c551ae6f64dac80f82ae2acdf
valmri <valmarmie87@outlook.com>
2022-06-04 23:57:49 +0200

    Création de l'index

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once './modele/manager/ManagerPrincipal.php';
+require_once './controleur/controleurPrincipal.php';
+
+if(isset($_GET['page'])) {
+    $controller = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
+} else {
+    $controller = "accueil";
+}
+
+$direction = controleurPrincipal($controller);
+
+// Chargement des vues principales
+require_once './vue/elements/head.php';
+require_once './vue/elements/header.php';
+require_once './vue/elements/menu.php';
+
+// Chargement du contrôleur qui chargera la vue
+require_once './controleur/'.$direction['chemin'];
+
+// Chargement du pied de page
+require_once './vue/elements/footer.php';
