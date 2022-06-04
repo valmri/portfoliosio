62e02ba9c9538fd0a2791f976827045dcd2c430f
valmri <valmarmie87@outlook.com>
2022-06-04 23:58:19 +0200

    Création de la page des pages

---
+++ controleur/public/c_page.php
@@ -0,0 +1,16 @@
+<?php
+// Importation des fichiers
+require_once './modele/manager/PageManager.php';
+require_once './modele/entite/Page.php';
+
+use manager\PageManager;
+
+// Récupération des données
+$pageManager = new PageManager();
+$page = $pageManager->getPageByCle($direction['cle']);
+
+
+
+require_once './vue/public/v_page.php';
+
+require_once './vue/elements/footer.php';
---
+++ vue/public/v_page.php
@@ -0,0 +1,2 @@
+<h1><?= $page->getTitre(); ?></h1>
+<?= html_entity_decode($page->getContenu()); ?>
