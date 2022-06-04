90f87b6b2882ee02dcb8005e63c6e01409c9b4aa
valmri <valmarmie87@outlook.com>
2022-06-04 23:56:44 +0200

    Création des éléments

---
+++ vue/elements/head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Prenom Nom - Fonction</title>
+    <link rel="shortcut icon" href="./assets/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="./assets/css/styles.css" />
+    <link rel="stylesheet" href="./assets/css/activite.css" />
+    <link rel="stylesheet" href="./assets/css/responsive.css">
+    <?php if(isset($_SESSION['connecte'])) : ?>
+    <link rel="stylesheet" href="./assets/css/admin.css" />
+    <?php endif; ?>
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"/>
+</head>
---
+++ vue/elements/menu.php
@@ -0,0 +1,30 @@
+<menu class="carte menu">
+    <ul class="menuPrincipal">
+        <li>
+            <a class="boutonMenu" href="?page=accueil">
+                <i class="las la-home"></i>
+                Accueil
+            </a>
+            <a class="boutonMenu" href="?page=projets">
+                <i class="las la-briefcase"></i>
+                Mes projets
+            </a>
+            <a class="boutonMenu" href="?page=parcours">
+                <i class="las la-map-signs"></i>
+                Mon parcours
+            </a>
+            <a class="boutonMenu" href="?page=veille">
+                <i class="las la-newspaper"></i>
+                Veille technologique
+            </a>
+            <a class="boutonMenu" target="_blank" href="?page=veille">
+                <i class="las la-graduation-cap"></i>
+                BTS SIO
+            </a>
+            <a class="boutonMenu" href="?page=contact">
+                <i class="las la-envelope"></i>
+                Contact
+            </a>
+        </li>
+    </ul>
+</menu>
