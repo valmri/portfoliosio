270283c991d4bd4912af3e68aebf0831749354e0
valmri <valmarmie87@outlook.com>
2022-06-08 00:36:15 +0200

    Direction vers les projets

--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -18,6 +18,7 @@ function controleurPrincipal(string $page){
     }
 
     $lesPages['projets'] =  "public/c_projets.php";
+    $lesPages['projet'] =  "public/c_projet.php";
 
     // Vérification de l'existence de la clé saisie
     if (array_key_exists($page , $lesPages )){
