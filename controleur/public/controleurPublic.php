a0dd3042711efbd5289a02fdf75c88d5a913f6ff
valmri <valmarmie87@outlook.com>
2022-06-08 02:10:30 +0200

    Création index public

---
+++ controleur/public/controleurPublic.php
@@ -0,0 +1,36 @@
+<?php
+require_once './modele/manager/PageManager.php';
+
+use manager\PageManager;
+
+function controleurPublic(string $page){
+
+    $pageManager = new PageManager();
+
+    $lesCles = $pageManager->getCles();
+
+    $lesPages = array();
+
+    foreach ($lesCles as $uneCle) {
+
+        $lesPages[$uneCle['cle']] =  "c_page.php";
+
+    }
+
+    $lesPages['projets'] =  "c_projets.php";
+    $lesPages['projet'] =  "c_projet.php";
+    $lesPages['connexion'] =  "c_connexion.php";
+
+    // Vérification de l'existence de la clé saisie
+    if (array_key_exists($page , $lesPages )){
+        $cle = $page;
+        $chemin = $lesPages[$page];
+    } else {
+        $cle = 'accueil';
+        $chemin = $lesPages['accueil'];
+    }
+
+    return ['cle' => $cle, 'chemin' => $chemin];
+
+}
+
---
+++ vue/public/index.php
@@ -0,0 +1,31 @@
+<?php
+require_once './modele/manager/ManagerPrincipal.php';
+require_once './controleur/public/controleurPublic.php';
+
+if(isset($_GET['page'])) {
+    $controller = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
+} else {
+    $controller = "accueil";
+}
+
+$direction = controleurPublic($controller);
+
+// Configuration de l'entête
+$moi = array(
+    'nom' => 'Nom',
+    'prenom' => 'Prenom',
+    'status' => 'Étudiant en informatique'
+);
+
+// Chargement des vues principales
+require_once './vue/elements/head.php';
+require_once './vue/elements/header.php';
+
+// Chargement du menu de façon automatique
+require_once './controleur/elements/c_menu.php';
+
+// Chargement du contrôleur qui chargera la vue
+require_once './controleur/public/'.$direction['chemin'];
+
+// Chargement du pied de page
+require_once './vue/elements/footer.php';
