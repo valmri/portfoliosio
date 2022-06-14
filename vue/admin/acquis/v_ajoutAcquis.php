2c2e9a0f3c83c14fc086d7a5c72a47d6ca0456ce
valmri <valmarmie87@outlook.com>
2022-06-14 23:30:17 +0200

    Design dashboard

--- vue/admin/acquis/v_ajoutAcquis.php
+++ vue/admin/acquis/v_ajoutAcquis.php
@@ -20,21 +20,21 @@
 
         <form action="#" method="post">
             <label for="projetId">Projet :</label>
-            <select name="projetId" id="projetSelect" required>
+            <select class="inputSpe" name="projetId" id="projetSelect" required>
                 <?php foreach ($lesProjets as $leProjet): ?>
                     <option value="<?php echo $leProjet['id']; ?>"><?php echo $leProjet['titre']; ?></option>
                 <?php endforeach; ?>
             </select><br/>
 
             <label for="activiteId">Activite :</label>
-            <select name="activiteId" id="activiteSelect" required>
+            <select class="inputSpe" name="activiteId" id="activiteSelect" required>
                 <?php foreach ($lesActivites as $uneActivite): ?>
                     <option value="<?php echo $uneActivite['id']; ?>" onclick="getCompetences(this.value)"><?php echo $uneActivite['intitule']; ?></option>
                 <?php endforeach; ?>
             </select><br/>
 
             <label for="competenceId">Competence :</label>
-            <select name="competenceId" id="competenceSelect" required>
+            <select class="inputSpe" name="competenceId" id="competenceSelect" required>
 
 
                 <?php foreach($lesCompetences as $uneCompetence) : ?>
@@ -46,7 +46,11 @@
             <label for="contenu">Contenu :</label>
             <textarea name="contenu" id="editor1" rows="10" cols="80"></textarea><br/>
 
-            <input class="bouton btnPost" type="submit">
+            <button class="btnAdmin btnPost" type="submit">
+                <i class="las la-save"></i>
+                Publier
+            </button>
+
         </form>
 
     </div>
