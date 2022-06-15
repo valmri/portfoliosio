14b1c2c3f791e9b7328fa9f589e480845dff63fc
valmri <valmarmie87@outlook.com>
2022-06-15 15:25:29 +0200

    Design admin

--- vue/admin/elements/head.php
+++ vue/admin/elements/head.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PortfolioSIO - Administration</title>
     <link rel="shortcut icon" href="./assets/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="./assets/css/initialisation.css" />
     <link rel="stylesheet" href="./assets/css/styles.css" />
     <link rel="stylesheet" href="./assets/css/admin.css" />
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"/>
